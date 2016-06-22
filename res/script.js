996e77b33527477c42d033bf6799c9d41800dbf5
Jonathan Lam <jlam55555@gmail.com>
2016-06-22 09:09:02 -0400

    Moved much of code to Jekyll format

--- res/script.js
+++ res/script.js
@@ -25,53 +25,42 @@ $(function() {
     reposition();
     $("div#menu").css({ height: win.height() });
   });
-
-  // get footer, header, and styles
-  $("head").append("<link rel='stylesheet' type='text/css' href='res/fa.css' /><link rel='stylesheet' type='text/css' href='res/style.css' />");
-  $.get("header.html", function(html) {
-    $("header").html(html);
-    // toggle menu
-    $(".menuButton").click(function() { $("div#menu").toggleClass("hidden").toggleClass("shadow"); });
-    doc.on("click", "*", function(event) {
-      event.stopPropagation();
-      if(!$(this).is("div#menu, div#menu *, .menuButton") && !$("div#menu").hasClass("hidden"))
-        $("div#menu").addClass("hidden").removeClass("shadow");
-    });
-    win.resize();
-    // dropdown code
-    $(".dropdown").each(function() {
-      $("#dropdown" + $(this).data("dropdown")).css({ top: $("nav#mainNav").position().top + $("nav#mainNav").height(), left: $(this).position().left });
-      $(this).hover(function() {
-        $(".dropdown").removeClass("linger");
-        $(this).addClass("linger");
-        $(".dropdownLinks").addClass("hidden");
-        $("#dropdown" + $(this).data("dropdown")).removeClass("hidden");
-      }, function() {
-        var dropdown = $("#dropdown" + $(this).data("dropdown")), dropdownTab = $(this);
-        doc.mousemove(function(event) {
-          var x = event.pageX, y = event.pageY, minX = dropdown.position().left, minY = $("nav#mainNav").position().top, maxX = minX + dropdown.width(), maxY = minY + $("nav#mainNav").height() + dropdown.height();
-          if(x < minX || x > maxX || y < minY || y > maxY) {
-            dropdown.addClass("hidden");
-            doc.off("mousemove");
-            dropdownTab.removeClass("linger");
-          }
-        });
+  // toggle menu
+  $(".menuButton").click(function() { $("div#menu").toggleClass("hidden").toggleClass("shadow"); });
+  doc.on("click", "*", function(event) {
+    event.stopPropagation();
+    if(!$(this).is("div#menu, div#menu *, .menuButton") && !$("div#menu").hasClass("hidden"))
+      $("div#menu").addClass("hidden").removeClass("shadow");
+  });
+  // dropdown code
+  $(".dropdown").each(function() {
+    $("#dropdown" + $(this).data("dropdown")).css({ top: $("nav#mainNav").position().top + $("nav#mainNav").height(), left: $(this).position().left });
+    $(this).hover(function() {
+      $(".dropdown").removeClass("linger");
+      $(this).addClass("linger");
+      $(".dropdownLinks").addClass("hidden");
+      $("#dropdown" + $(this).data("dropdown")).removeClass("hidden");
+    }, function() {
+      var dropdown = $("#dropdown" + $(this).data("dropdown")), dropdownTab = $(this);
+      doc.mousemove(function(event) {
+        var x = event.pageX, y = event.pageY, minX = dropdown.position().left, minY = $("nav#mainNav").position().top, maxX = minX + dropdown.width(), maxY = minY + $("nav#mainNav").height() + dropdown.height();
+        if(x < minX || x > maxX || y < minY || y > maxY) {
+          dropdown.addClass("hidden");
+          doc.off("mousemove");
+          dropdownTab.removeClass("linger");
+        }
       });
     });
   });
-  $.get("footer.html", function(html) {
-    $("footer").html(html);
-    reposition();
-    // popups to share
-    $("body").on("click", ".shareButtonLink", function() {
-      window.open($(this).attr("href"), "", "menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=500,width=500");
-      return false;
-    });
-    $("#facebookShareButton").attr({ href: "https://www.facebook.com/sharer/sharer.php?u=" + encodeURIComponent(window.location.href) });
-    $("#twitterShareButton").attr({ href: "https://twitter.com/intent/tweet?text=" + encodeURIComponent("Come visit at Nutmeg Bowl for the best bowling in Fairfield County! " + window.location.href) });
-    $("#googlePlusShareButton").attr({ href: "https://plus.google.com/share?url=" + encodeURIComponent(window.location.href) });
-
-    win.resize();
+  // popups to share
+  $("body").on("click", ".shareButtonLink", function() {
+    window.open($(this).attr("href"), "", "menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=500,width=500");
+    return false;
   });
+  $("#facebookShareButton").attr({ href: "https://www.facebook.com/sharer/sharer.php?u=" + encodeURIComponent(window.location.href) });
+  $("#twitterShareButton").attr({ href: "https://twitter.com/intent/tweet?text=" + encodeURIComponent("Come visit at Nutmeg Bowl for the best bowling in Fairfield County! " + window.location.href) });
+  $("#googlePlusShareButton").attr({ href: "https://plus.google.com/share?url=" + encodeURIComponent(window.location.href) });
+
+  win.resize();
 
 });
